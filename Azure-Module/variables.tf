5bc04394dd99d982635a46bc9f0ad83dac7e383a
CraftMatic <alexander.craft.23@gmail.com>
2025-11-03 11:07:58 -0500

    Update variables.tf

--- Azure-Module/variables.tf
+++ Azure-Module/variables.tf
@@ -1,1 +1,12 @@
 #Input Variables
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group 22"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "East US"
+}
